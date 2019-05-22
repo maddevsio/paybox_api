3dd14ea13ec37aeee073b3b5dcea2f119d142fc7
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-05-23 02:15:22 +0400

    Update authorization

--- lib/paybox_api/v4/client.rb
+++ lib/paybox_api/v4/client.rb
@@ -9,19 +9,25 @@ module PayboxApi
         @secret_key = secret_key
       end
 
-      def init_payment(**params)
+      def payments(**params)
+        required_keys = [ :order, :amount, :currency, :description, :uuid ]
+        unless required_keys.all? { |key| params.key? key }
+          raise "Payments method required keys: #{required_keys.join(', ')}"
+        end
         puts url
         uri = URI.parse url
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
         req = Net::HTTP::Post.new(uri.to_s)
-        req['Merchant ID'] = @merchant_id
-        req['Secret Key'] = @secret_key
+        req.basic_auth @merchant_id, @secret_key
         req['X-Idempotency-Key'] = params[:uuid]
         json = params.to_json
         res = http.request req, json
         puts json
         puts res.body
+        req.each_header do |name, value|
+          puts "#{name} #{value}"
+        end
         res.body
       end
 
