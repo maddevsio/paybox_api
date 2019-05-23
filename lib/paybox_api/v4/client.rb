22cfa7377a1a6e5a599ce8fc725f2580ef1a39b6
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2019-05-24 01:24:49 +0400

    Working sending payments with v 4.0

--- lib/paybox_api/v4/client.rb
+++ lib/paybox_api/v4/client.rb
@@ -14,21 +14,17 @@ module PayboxApi
         unless required_keys.all? { |key| params.key? key }
           raise "Payments method required keys: #{required_keys.join(', ')}"
         end
-        puts url
         uri = URI.parse url
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
         req = Net::HTTP::Post.new(uri.to_s)
         req.basic_auth @merchant_id, @secret_key
         req['X-Idempotency-Key'] = params[:uuid]
+        params.except! :uuid
+        params[:order] = params[:order].to_s
+        params[:expires_at] ||= (DateTime.now + 1.day).to_s
         json = params.to_json
-        res = http.request req, json
-        puts json
-        puts res.body
-        req.each_header do |name, value|
-          puts "#{name} #{value}"
-        end
-        res.body
+        http.request(req, json).body
       end
 
       private
